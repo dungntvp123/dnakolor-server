771bef1fcc8681327f7bc84fa6ae8b070492da7f
DungNT <tridungtin8@gmail.com>
2024-06-18 17:59:33 +0700

    update entity

---
+++ src/main/java/com/dnakolor/account/data/entity/Account.java
@@ -0,0 +1,47 @@
+package com.dnakolor.account.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Account implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private boolean isPurchase;
+    private String roles;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String role : roles.split(",")) {
+            authorities.add((GrantedAuthority) () -> role);
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return username;
+    }
+
+    @Override
+    public String getUsername() {
+        return password;
+    }
+}
